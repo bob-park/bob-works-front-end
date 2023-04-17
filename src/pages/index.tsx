355a9d020078fd5605f0d2a5ac4ce5cd72a8fcaa
Bob Park <hwpark11@kakao.com>
2023-04-18 01:04:12 +0900

    feat: 로그인 시 back-end 중 client service 로 이동하도록 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
+import { useLayoutEffect } from 'react';
+import axios from 'axios';
+
+const client = axios.create({
+  withCredentials: true,
+});
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  useLayoutEffect(() => {
+    client.get('/api/home').then((res) => console.log(res.data));
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
