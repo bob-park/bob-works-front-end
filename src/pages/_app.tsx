92a941a63155b02814c675f6b6cbb7399e609c3a
Bob Park <hwpark@malgn.com>
2023-04-19 09:45:04 +0900

    feat: side bar component 분리 및 footer 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
+import Sidebar from '@/component/SideBar';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
-import { Avatar, Label } from 'flowbite-react';
+import { Footer } from 'flowbite-react';
+
+const { Copyright, LinkGroup, Link } = Footer;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -10,23 +13,28 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Bob Works</title>
       </Head>
-      <div className="flex h-full p-10 w-full">
-        <div className="flex-initial w-64 min-w-min md:min-w-0">
+      <div className="flex p-10 w-full">
+        {/* TODO 나중에 이거 꼭 반응형으로 바꾸잣 */}
+        <div className="flex-initial w-1/3 max-w-[300px] min-w-[300px] border-r-2 border-solid">
           <div className="grid grid-cols-1">
-            {/* Avatar */}
-            <div className="grid justify-start">
-              <div className="mt-10 ">
-                <Avatar rounded size="md"></Avatar>
-              </div>
-              <div className="mt-10">name</div>
-              <div className="mt-1">sub name</div>
-            </div>
+            <Sidebar />
           </div>
         </div>
-        <div className="flex-initial w-full">
+
+        <div className="flex-initial w-full p-20">
           <Component {...pageProps} />
         </div>
       </div>
+
+      <Footer container={true}>
+        <Copyright href="#" by="by Bob park" year={new Date().getFullYear()} />
+        <LinkGroup>
+          <Link href="#">About</Link>
+          <Link href="#">Privacy Policy</Link>
+          <Link href="#">Licensing</Link>
+          <Link href="#">Contact</Link>
+        </LinkGroup>
+      </Footer>
     </div>
   );
 }
