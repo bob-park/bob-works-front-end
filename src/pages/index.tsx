bb441bc92fc92c7ac8eabe335f0d37df40a8d3d5
Bob Park <hwpark11@kakao.com>
2023-04-20 20:59:09 +0900

    feat: 대시보드 ui 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,65 @@
+import { Card } from 'flowbite-react';
+
 export default function Home() {
-  return <main>index</main>;
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      <Card>
+        <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
+          최근 결제 문서
+        </h5>
+        <div className="flow-root">
+          <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+            <li className="py-3 sm:py-4">
+              <div className="flex items-center space-x-4">
+                <div className="min-w-0 flex-1">
+                  <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
+                    결제 대기 중
+                  </p>
+                </div>
+                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                  12
+                </div>
+              </div>
+            </li>
+            <li className="py-3 sm:py-4">
+              <div className="flex items-center space-x-4">
+                <div className="min-w-0 flex-1">
+                  <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
+                    결제 진행 중
+                  </p>
+                </div>
+                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                  5
+                </div>
+              </div>
+            </li>
+            <li className="py-3 sm:py-4">
+              <div className="flex items-center space-x-4">
+                <div className="min-w-0 flex-1">
+                  <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
+                    결제 완료
+                  </p>
+                </div>
+                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                  2
+                </div>
+              </div>
+            </li>
+            <li className="py-3 sm:py-4">
+              <div className="flex items-center space-x-4">
+                <div className="min-w-0 flex-1">
+                  <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
+                    결제 반려
+                  </p>
+                </div>
+                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                  3
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </Card>
+    </div>
+  );
 }
