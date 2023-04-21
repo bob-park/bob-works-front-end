47424f35c5aa5c07e296f684ac3bf964434c4bde
Bob Park <hwpark11@kakao.com>
2023-04-21 19:43:48 +0900

    feat: 대시보드에 사용자 휴가정보 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -60,6 +60,96 @@ export default function Home() {
           </ul>
         </div>
       </Card>
+      <Card>
+        <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
+          휴가 정보
+        </h5>
+        <div className="flow-root">
+          <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+            <li className="py-3 sm:py-4">
+              <div className="flex items-center space-x-4">
+                <div className="min-w-0 flex-1">
+                  <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
+                    총 연차 수
+                  </p>
+                </div>
+                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                  15
+                </div>
+              </div>
+            </li>
+            <li className="py-3 sm:py-4">
+              <div className="flex items-center space-x-4">
+                <div className="min-w-0 flex-1">
+                  <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
+                    사용한 연차 수
+                  </p>
+                </div>
+                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                  5
+                </div>
+              </div>
+            </li>
+            <li className="py-3 sm:py-4">
+              <div className="flex items-center space-x-4">
+                <div className="min-w-0 flex-1">
+                  <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
+                    남은 연차 수
+                  </p>
+                </div>
+                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                  10
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </Card>
+      <Card>
+        <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
+          대체휴가 정보
+        </h5>
+        <div className="flow-root">
+          <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+            <li className="py-3 sm:py-4">
+              <div className="flex items-center space-x-4">
+                <div className="min-w-0 flex-1">
+                  <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
+                    총 대체휴가 수
+                  </p>
+                </div>
+                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                  2.5
+                </div>
+              </div>
+            </li>
+            <li className="py-3 sm:py-4">
+              <div className="flex items-center space-x-4">
+                <div className="min-w-0 flex-1">
+                  <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
+                    사용한 대체휴가 수
+                  </p>
+                </div>
+                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                  1
+                </div>
+              </div>
+            </li>
+            <li className="py-3 sm:py-4">
+              <div className="flex items-center space-x-4">
+                <div className="min-w-0 flex-1">
+                  <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
+                    남은 대체휴가 수
+                  </p>
+                </div>
+                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                  1.5
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </Card>
     </div>
   );
 }
