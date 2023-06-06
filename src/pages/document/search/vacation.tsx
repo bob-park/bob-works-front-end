afad6d70cf104da454ebdba412f72205ed5b1617
Bob Park <hwpark11@kakao.com>
2023-06-06 19:46:44 +0900

    feat: 휴가계 취소 시 confirm 추가

--- src/pages/document/search/vacation.tsx
+++ src/pages/document/search/vacation.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import { Button, Card, Spinner } from 'flowbite-react';
+import { FormEvent, useEffect, useState } from 'react';
+import { Button, Card, Modal, Spinner } from 'flowbite-react';
 import { IoChevronBackSharp } from 'react-icons/io5';
 import { FiDownload, FiPrinter } from 'react-icons/fi';
 import { MdOutlineCancel } from 'react-icons/md';
@@ -26,6 +26,7 @@ export default function VacationDocumentList() {
 
   // useState
   const [isPrinting, setIsPrinting] = useState<boolean>(false);
+  const [showConfirmCancel, setShowConfirmCancel] = useState<boolean>(false);
 
   // useEffect
   useEffect(() => {
@@ -67,7 +68,9 @@ export default function VacationDocumentList() {
 
   const handlePrint = () => {};
 
-  const handleCancel = () => {
+  const handleCancel = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
     const { documentId } = router.query;
 
     if (!documentId) {
@@ -85,78 +88,102 @@ export default function VacationDocumentList() {
   };
 
   return (
-    <div className="w-full h-full">
-      <div className="grid grid-cols-1">
-        {/* title */}
-        <div>
-          <div className="inline-block">
-            <Button
-              size="xl"
-              gradientDuoTone="whiteToWhite"
-              onClick={handleBackDrop}
-            >
-              <IoChevronBackSharp />
-            </Button>
+    <>
+      <div className="w-full h-full">
+        <div className="grid grid-cols-1">
+          {/* title */}
+          <div>
+            <div className="inline-block">
+              <Button
+                size="xl"
+                gradientDuoTone="whiteToWhite"
+                onClick={handleBackDrop}
+              >
+                <IoChevronBackSharp />
+              </Button>
+            </div>
+
+            <span className="text-xl font-semibold ml-2">상세</span>
           </div>
 
-          <span className="text-xl font-semibold ml-2">상세</span>
-        </div>
+          {/* buttons */}
+          <div>
+            <div className="grid grid-cols-3 gap-10 mt-3 justify-end">
+              <Button color="dark" onClick={handlePrint} disabled>
+                {isPrinting ? (
+                  <>
+                    <Spinner
+                      className="mr-2 h-5 w-5"
+                      color="info"
+                      aria-label="Info spinner example"
+                    />
+                    인쇄중
+                  </>
+                ) : (
+                  <>
+                    <FiPrinter className="mr-2 h-5 w-5" size="" />
+                    인쇄
+                  </>
+                )}
+              </Button>
 
-        {/* buttons */}
-        <div>
-          <div className="grid grid-cols-3 gap-10 mt-3 justify-end">
-            <Button color="dark" onClick={handlePrint} disabled>
-              {isPrinting ? (
-                <>
-                  <Spinner
-                    className="mr-2 h-5 w-5"
-                    color="info"
-                    aria-label="Info spinner example"
-                  />
-                  인쇄중
-                </>
-              ) : (
-                <>
-                  <FiPrinter className="mr-2 h-5 w-5" size="" />
-                  인쇄
-                </>
-              )}
-            </Button>
+              <Button
+                onClick={handleCapture}
+                disabled={vacationDocument.status === 'CANCEL'}
+              >
+                <FiDownload className="mr-2 h-5 w-5" />
+                PDF 다운로드
+              </Button>
 
-            <Button
-              onClick={handleCapture}
-              disabled={vacationDocument.status === 'CANCEL'}
-            >
-              <FiDownload className="mr-2 h-5 w-5" />
-              PDF 다운로드
-            </Button>
-
-            <Button
-              onClick={handleCancel}
-              color="failure"
-              disabled={vacationDocument.status === 'CANCEL'}
-            >
-              <MdOutlineCancel className="mr-2 h-5 w-5" />
-              취소
-            </Button>
+              <Button
+                onClick={() => setShowConfirmCancel(true)}
+                color="failure"
+                disabled={vacationDocument.status === 'CANCEL'}
+              >
+                <MdOutlineCancel className="mr-2 h-5 w-5" />
+                취소
+              </Button>
+            </div>
           </div>
-        </div>
 
-        {/* contents */}
-        <Card className="mt-4 relative">
-          {vacationDocument.status === 'CANCEL' && (
-            <div className="absolute top-0 bottom-0 w-full h-ful">
-              <div className="grid place-content-center w-full h-full opacity-50">
-                <div className="text-red-700 font-black text-9xl tracking-widest -rotate-45 border-8 border-solid border-red-700 rounded p-10">
-                  취 소
+          {/* contents */}
+          <Card className="mt-4 relative">
+            {vacationDocument.status === 'CANCEL' && (
+              <div className="absolute top-0 bottom-0 w-full h-ful">
+                <div className="grid place-content-center w-full h-full opacity-50">
+                  <div className="text-red-700 font-black text-9xl tracking-widest -rotate-45 border-8 border-solid border-red-700 rounded p-10">
+                    취 소
+                  </div>
                 </div>
               </div>
-            </div>
-          )}
+            )}
 
-          <VacationDocument document={vacationDocument} />
-        </Card>
+            <VacationDocument document={vacationDocument} />
+          </Card>
+        </div>
       </div>
-    </div>
+      <Modal
+        show={showConfirmCancel}
+        onClose={() => setShowConfirmCancel(false)}
+        dismissible
+      >
+        <form onSubmit={handleCancel}>
+          <Modal.Header>취소 할꺼야?</Modal.Header>
+          {/* <Modal.Body>
+            <div>
+              <h2 className="text-xl font-semibold ml-2">취소 할꺼야?</h2>
+            </div>
+          </Modal.Body> */}
+          <Modal.Footer className="">
+            <div className="grid grid-cols-2 gap-2 justify-end items-end w-full">
+              <Button color="gray" onClick={() => setShowConfirmCancel(false)}>
+                안할꺼야
+              </Button>
+              <Button type="submit">할꺼야</Button>
+            </div>
+          </Modal.Footer>
+        </form>
+      </Modal>
+    </>
   );
 }
